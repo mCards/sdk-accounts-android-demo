87c35839b930128fc106bca90785f23fdacab5bc
jnewgard <44853169+jnewgard@users.noreply.github.com>
2024-12-17 13:17:56 -0800

    Create project (#1)
    * Create initial project
    
    * Update sdk version
    
    * Update kotlinc
    
    * Add github configs
    
    * Update sdk + fixes + add readme
    
    * Update sdk

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# sdk-accounts-android-demo
+# mCards Android Accounts SDK Demo App
+
+The mCards android Accounts SDK encapsulates the following functionality:
+
+1. Link bank accounts
+2. Link credit accounts
+3. Deactivate accounts
+4. Reorder account priority
+
+# Usage
+Implementing apps MUST override this string value for auth0 to work:
+
+<string name="auth0_domain">your value here</string>
+
+Theis value is gotten from the mCards team after setting up the client's auth0 instance.
+
+You must then also update the manifest placeholders in the build.gradle file:
+
+e.g. addManifestPlaceholders(mapOf("auth0Domain" to "@string/auth0_domain", "auth0Scheme" to "your app ID"))
+
+
+# Importing the Auth SDK
+Add the following to your module-level build.gradle:
+
+Groovy:
+```
+implementation "com.mcards.sdk:accounts:$latestVersion"
+```
+
+Kotlin:
+```
+implementation("com.mcards.sdk:accounts:$latestVersion")
+```
+
+And the following to the project settings.gradle:
+```
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+
+        maven {
+            url = uri("https://maven.pkg.github.com/Wantsa/sdk-accounts-android")
+            credentials {
+                username = GITHUB_USERNAME
+                password = GITHUB_TOKEN
+            }
+        }
+    }
+}
+```
+
+# Documentation
+\\\\\Add documentation links here/////
---
+++ app/src/main/java/com/mcards/sdk/accounts/demo/DemoFragment.kt
@@ -0,0 +1,136 @@
+package com.mcards.sdk.accounts.demo
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG
+import com.google.android.material.snackbar.Snackbar
+import com.mcards.sdk.accounts.AccountsSdk
+import com.mcards.sdk.accounts.AccountsSdkProvider
+import com.mcards.sdk.accounts.demo.databinding.FragmentDemoBinding
+import com.mcards.sdk.accounts.model.Account
+import com.mcards.sdk.auth.AuthSdk
+import com.mcards.sdk.auth.AuthSdkProvider
+import com.mcards.sdk.auth.model.auth.User
+import com.mcards.sdk.core.model.AuthTokens
+import com.mcards.sdk.core.network.SdkResult
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.SingleObserver
+import io.reactivex.rxjava3.disposables.Disposable
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class DemoFragment : Fragment() {
+
+    private var _binding: FragmentDemoBinding? = null
+    private val binding get() = _binding!!
+
+    private var userPhoneNumber = ""
+    private var accessToken = ""
+    private var idToken = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDemoBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val loginCallback = object : AuthSdk.LoginCallback {
+            override fun onSuccess(
+                user: User,
+                tokens: AuthTokens,
+                regionChanged: Boolean,
+                cardId: String?
+            ) {
+                accessToken = tokens.accessToken
+                idToken = tokens.idToken
+                userPhoneNumber = user.userClaim.phoneNumber
+                initAccountsSdk()
+            }
+
+            override fun onFailure(message: String) {
+                activity?.runOnUiThread {
+                    Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
+                }
+            }
+        }
+
+        val authSdk = AuthSdkProvider.getInstance()
+        binding.loginBtn.setOnClickListener {
+            if (userPhoneNumber.isBlank()) {
+                authSdk.login(requireContext(), loginCallback)
+            } else {
+                authSdk.login(requireContext(), userPhoneNumber, loginCallback)
+            }
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun initAccountsSdk() {
+        val accountsSdk = AccountsSdkProvider.getInstance()
+
+        accountsSdk.init(
+            requireContext(),
+            accessToken,
+            debug = true,
+            useFirebase =  false,
+            object : AccountsSdk.InvalidTokenCallback {
+                override fun onTokenInvalid(): String {
+                    return AuthSdkProvider.getInstance().refreshTokens().accessToken
+                }
+            })
+
+        accountsSdk.getAccounts()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object : SingleObserver<SdkResult<Array<Account>>> {
+                override fun onSubscribe(d: Disposable) {
+                    activity?.runOnUiThread {
+                        binding.progressbar.visibility = View.VISIBLE
+                    }
+                }
+
+                override fun onError(e: Throwable) {
+                    activity?.runOnUiThread {
+                        binding.progressbar.visibility = View.GONE
+                        Snackbar.make(requireView(), e.message!!, LENGTH_LONG).show()
+                    }
+                }
+
+                override fun onSuccess(t: SdkResult<Array<Account>>) {
+                    activity?.runOnUiThread {
+                        binding.progressbar.visibility = View.GONE
+                    }
+
+                    t.result?.let {
+                        activity?.runOnUiThread {
+                            MaterialAlertDialogBuilder(requireContext())
+                                .setTitle("Success")
+                                .setMessage("Success! Fetched " + it.size + " Accounts. Debug" +
+                                        " to inspect the data.")
+                                .setPositiveButton("Ok") { dialog, _ ->
+                                    dialog.dismiss()
+                                }.create().show()
+                        }
+                    } ?: t.errorMsg?.let {
+                        activity?.runOnUiThread {
+                            Snackbar.make(requireView(), it, LENGTH_LONG).show()
+                        }
+                    }
+                }
+            })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
